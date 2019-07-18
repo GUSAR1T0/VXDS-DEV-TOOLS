29dac0ffe465bba8296b4df31766cd24e3d64294
Roman Mashenkin <xromash@vxdesign.store>
2019-07-18 05:06:44 +0300

    Improve route generation
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

--- DevTools/UnifiedPortal/Static/src/plugins/router.js
+++ DevTools/UnifiedPortal/Static/src/plugins/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/views/Home.vue";
-import Authorization from "@/views/Authorization.vue";
 
 Vue.use(Router);
 
@@ -20,7 +19,7 @@ export default new Router({
         {
             path: "/auth",
             name: "authorization",
-            component: Authorization,
+            component: () => import(/* webpackChunkName: "authorization" */ "../views/Authorization.vue"),
             meta: {
                 pageName: "Authorization"
             }
