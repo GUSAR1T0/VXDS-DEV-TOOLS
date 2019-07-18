f0dcd1fdcd420e3f3422389c763388b0f35a16d9
Roman Mashenkin <xromash@vxdesign.store>
2019-07-19 01:44:12 +0300

    Rename folders
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

--- ExtTools/CamundaServer/src/main/java/store/vxdesign/exttools/camunda/Application.java
+++ ExtTools/CamundaServer/src/main/java/store/vxdesign/exttools/camunda/Application.java
@@ -1,4 +1,4 @@
-package store.vxdesign.devtools.camunda;
+package store.vxdesign.exttools.camunda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
